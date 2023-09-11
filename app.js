d592673656eac05cf86a7d7f2218c1f549c745af
Nathan Cosgrove <98294668+Ncosgrove1@users.noreply.github.com>
2023-09-11 01:53:00 -0500

    Added String Variable

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const b = 2 ;
 
 let c = 3 ; 
 
-let x = (25 +30) * c ;
+let x = (25 +30) * c ; // Math
 
-let y = b * c ;
+let y = b * c ; // Math 
+
+let text1 ="My favorite color is" ; // String Variable
 
 
-
-console.log("The variables are listed below", '\n', "var a = 1 ", '\n' , "const b = 2", '\n', "let c = 3", '\n', "let x = (25+30) * c", '\n' 
-    ,"Therefore x =",x,'\n', "And y =", y) ;
+console.log("The variables are listed below", '\n', "var a = 1 ", '\n' , "const b = 2", '\n', "let c = 3", '\n', "let x = (25+30) * c", '\n', 
+    "let y = b * c",'\n', "Therefore x =",x,'\n', "And y =", y, '\n', "Now onto string variables!", '\n', "let text1 = My favorite color", '\n',"Therfore text1+= the answer below", '\n', text1 +=" red",
+) ;
