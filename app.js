dca37e1d415fa6ee5c238d21733a1f44465182cf
Nathan Cosgrove <98294668+Ncosgrove1@users.noreply.github.com>
2023-09-11 02:30:34 -0500

    Added Block Variable

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ let text1 ="My favorite color is" ; // String Variable
 console.log("The variables are listed below", '\n', "var a = 1 ", '\n' , "const b = 2", '\n', "let c = 3", '\n', "let x = (25+30) * c", '\n', 
     "let y = b * c",'\n', "Therefore x =",x,'\n', "And y =", y, '\n', "Now onto string variables!", '\n', "let text1 = My favorite color", '\n',"Therfore text1+= the answer below", '\n', text1 +=" red",
 ) ;
+
+let square = "Horray!" ; {
+console.log("This is my block variable", '\n', "Hip Hip", square , '\n', "let square = Horray! {console.log...} ")    
+}
