72dd35dfc7c44f59d4e67920e2eaf43e18788876
Nathan Cosgrove <98294668+Ncosgrove1@users.noreply.github.com>
2023-09-11 01:35:02 -0500

    Added variables and mathematical operations

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var glob = 100 ;
+var a = 1 ;
 
-var name = Nathan ;
+const b = 2 ; 
+
+let c = 3 ; 
+
+let x = (25 +30) * c ;
+
+let y = b * c ;
 
 
 
-
-console.log("How long have you been coding, professor?") ;
+console.log("The variables are listed below", '\n', "var a = 1 ", '\n' , "const b = 2", '\n', "let c = 3", '\n', "let x = (25+30) * c", '\n' 
+    ,"Therefore x =",x,'\n', "And y =", y) ;
